ecc1b0e9a24eff1e1fecee646a1d90740ce95a96
Brody <rufengch@gmail.com>
2015-11-18 14:38:47 -0800

    ensure 60fps when scrolling in pizza.html

--- views/js/main.js
+++ views/js/main.js
@@ -502,10 +502,14 @@ function updatePositions() {
   frame++;
   window.performance.mark("mark_start_frame");
 
+  var phase = [];
+  for (var i = 0; i < 5; i++) {
+    phase.push(Math.sin((document.body.scrollTop / 1250) + (i % 5)));
+  }
   var items = document.querySelectorAll('.mover');
   for (var i = 0; i < items.length; i++) {
-    var phase = Math.sin((document.body.scrollTop / 1250) + (i % 5));
-    items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
+  //  var phase = Math.sin((document.body.scrollTop / 1250) + (i % 5));
+    items[i].style.left = items[i].basicLeft + 100 * phase[i % 5] + 'px';
   }
 
   // User Timing API to the rescue again. Seriously, it's worth learning.
