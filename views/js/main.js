96d9f4ac6811e7ef160e61aaaebe5ae1d1d01453
Brody <rufengch@gmail.com>
2015-11-18 18:26:56 -0800

    rewrite README.md

--- views/js/main.js
+++ views/js/main.js
@@ -495,7 +495,6 @@ function updatePositions() {
   }
   var items = document.querySelectorAll('.mover');
   for (var i = 0; i < items.length; i++) {
-  //  var phase = Math.sin((document.body.scrollTop / 1250) + (i % 5));
     items[i].style.left = items[i].basicLeft + 100 * phase[i % 5] + 'px';
   }
 
